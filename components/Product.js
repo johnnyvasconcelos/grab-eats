0ce5e93a86b17f00c0b82391930fcd1daab91ea2
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-02-27 13:13:00 -0300

    página de produtos concluída mobile first

--- components/Product.js
+++ components/Product.js
@@ -2,16 +2,20 @@ const Product = () => {
   return (
     <>
       <article className="product">
-        <h3 className="product__title">McOferta Média Big Mac Duplo</h3>
-        <p className="product__ingredients">
-          Dois hambúrgueres de carne 100% bovina, méquinese, molho Honey&Fire,
-          molho lácteo com queijo, pão tipo brioche.
-        </p>
-        <div className="price">
-          R$ <span>39,90</span>
-        </div>
-        <div className="product__image">
-          <img src="/images/produto.jpg" alt="product image" />
+        <div className="container flex">
+          <div className="product__text">
+            <h3 className="product__title">McOferta Média Big Mac Duplo</h3>
+            <p className="product__ingredients">
+              Dois hambúrgueres de carne 100% bovina, méquinese, molho
+              Honey&Fire, molho lácteo com queijo, pão tipo brioche.
+            </p>
+            <div className="price">
+              R$ <span>39,90</span>
+            </div>
+          </div>
+          <div className="product__image">
+            <img src="/images/produto.jpg" alt="product image" />
+          </div>
         </div>
       </article>
     </>
--- pages/cardapio/index.js
+++ pages/cardapio/index.js
@@ -6,34 +6,38 @@ function Cardapio() {
     <>
       <Header />
       <main className="offers">
-        <header className="offers__header menu-background">
+        <header className="offers__header">
           <div className="container flex">
             <div className="flex offers__header--info">
               <img src="/images/logo.png" alt="logo" />
               <div className="offers__header--title">
-                <h1 className="offers__title">Grap Eats</h1>
+                <h1 className="offers__title">Grab Eats</h1>
                 <p className="offers__description">Fast Food</p>
               </div>
             </div>
-            <div className="reviews">
-              <span>star</span>5.0
+            <div className="reviews flex btn-min">
+              <img src="/images/star-review.svg" alt="star review svg icon" />
+              5.0
             </div>
           </div>
-          <div className="flex time container">
-            <span>relógio</span>Aberto
+          <div className="flex time container open">
+            <img src="/images/clock-green.svg" alt="clock time icon svg" />
+            <span>Aberto</span>
           </div>
         </header>
-        <div className="offers__categories">
-          <div className="container">
-            <span className="check">Lançamentos</span>
-            <span>Lanches</span>
-            <span>Fritas</span>
-            <span>Bebidas</span>
-            <span>Sobremesas</span>
+        <div className="offers__categories no-scroll">
+          <div className="container flex">
+            <span className="category check btn-min">Lançamentos</span>
+            <span className="category btn-min">Lanches</span>
+            <span className="category btn-min">Fritas</span>
+            <span className="category btn-min">Bebidas</span>
+            <span className="category btn-min">Sobremesas</span>
           </div>
         </div>
-        <section className="offers__products container">
-          <h2>Lançamentos</h2>
+        <section className="offers__products">
+          <div className="container">
+            <h2>Lançamentos</h2>
+          </div>
           <Product />
           <Product />
           <Product />
