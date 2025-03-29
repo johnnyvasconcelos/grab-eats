428e056df2b0ebd2d5c7681dc526fd09473d3d7b
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-03-29 14:46:30 -0300

    iniciando produtos painel

--- pages/painel-admin/cardapio/index.js
+++ pages/painel-admin/cardapio/index.js
@@ -5,6 +5,7 @@ import HeaderAdmin from "../../../components/HeaderAdmin";
 import Head from "next/head";
 const cardapio = () => {
   const [nomeRestaurante, setNomeRestaurante] = useState("");
+  const [produtos, setProdutos] = useState([]);
   useEffect(() => {
     fetch("/api/restaurante")
       .then((res) => res.json())
@@ -12,6 +13,22 @@ const cardapio = () => {
         setNomeRestaurante(data.nome_restaurante);
       })
       .catch(() => setError("Erro ao fazer requisição"));
+  }, []);
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const response = await fetch("/api/produtos");
+        const data = await response.json();
+        if (response.ok) {
+          setProdutos(data);
+        } else {
+          console.error("Erro ao buscar produtos:", data.error);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      }
+    };
+    fetchProdutos();
   }, []);
   return (
     <>
@@ -32,6 +49,34 @@ const cardapio = () => {
                   Cardápio &nbsp;&gt;&nbsp; Início
                 </span>
               </h2>
+              <div className={styles.menuRestaurant}>
+                {produtos.map((produto) => (
+                  <article
+                    key={produto.id}
+                    className={styles.menuRestaurantItem}
+                  >
+                    <div className={styles.menuRestaurantImage}>
+                      <img
+                        src={`/images/${produto.foto}`}
+                        alt={produto.nome_produto}
+                      />
+                    </div>
+                    <div className={styles.menuRestaurantText}>
+                      <h2>{produto.nome_produto}</h2>
+                      <p>{produto.descricao}</p>
+                      <p>{produto.ingredientes}</p>
+                      <div className={styles.infoProduct}>
+                        <h3>{produto.categoria_produto}</h3>
+                        <strong>{produto.preco}</strong>
+                      </div>
+                    </div>
+                    <div className={styles.btns}>
+                      <button>Editar</button>
+                      <button>Remover</button>
+                    </div>
+                  </article>
+                ))}
+              </div>
             </section>
           </div>
         </div>
